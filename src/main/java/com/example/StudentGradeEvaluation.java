af99447898bf738234f5787442f8b2545e039f64
smk4411 <saimanojkumar63@gmail.com>
2024-06-21 11:17:31 +0530

    student_details_manoj

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -1,28 +1,20 @@
-package com.example;
 
 import java.util.Scanner;
 
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
+        String name = "manoj";
+        int age = 28;
+        double grade = 88.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("grade: " + grade
+        );
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter their name
         System.out.print("Enter your name: ");
-        
-
-        // Prompt the user to enter their age
-        
-
-        // Prompt the user to enter their exam score
-        
-
-        // Determine the grade
-        String grade;
-        
-
-        // Print the student's details
-        
-
+        String inputName = scanner.nextLine();
+        System.out.println("Student details, " + inputName + "!");
        
     }
 }
